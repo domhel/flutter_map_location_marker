92deede8ce11e212a87dcfa7dc9a537e8d8a0166
Dominik Helfenstein <46935291+domhel@users.noreply.github.com>
2024-08-30 21:53:50 +0200

    use flutter_compass_v2 to fix dependency issue

--- lib/src/data/data_stream_factory.dart
+++ lib/src/data/data_stream_factory.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter_compass/flutter_compass.dart';
+import 'package:flutter_compass_v2/flutter_compass_v2.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 
